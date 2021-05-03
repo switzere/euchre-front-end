7569c019f61c3229daddbfac7edc97d634c2fb4a
switzere <switzere@uoguelph.ca>
2021-05-02 20:22:10 -0400

    Updated front-end to have fields that can populate a post request, this should be used for playing cards in the future but for now it draws specific cards

--- src/components/GameFront.js
+++ src/components/GameFront.js
@@ -8,7 +8,11 @@ class GameFront extends Component {
     this.state = {
       datas: '',
       cards: '',
-      posts: ''
+      posts: '',
+      card: {
+        suit: props.suit,
+        value: props.value
+      }
     }
   }
 
@@ -33,10 +37,24 @@ class GameFront extends Component {
           console.log(error)
         })
 
-    axios.post('http://localhost:3001/play', 'just a string',
+  }
+
+  testPost() {
+
+    var suit = parseInt(document.getElementById('suit').value);
+    var value = parseInt(document.getElementById('value').value);
+    console.log("HELLO");
+    console.log(suit);
+    console.log(value);
+
+
+    const json = JSON.stringify({"Suit":suit,"Value":value})
+    console.log(json)
+    axios.post('http://localhost:3001/play', json,
       {
         headers: {
-          'playerID': 'abc123'
+          'playerID': 'abc123',
+          'Content-Type': 'application/json'
         }
       }
     )
@@ -56,10 +74,18 @@ class GameFront extends Component {
     const { posts } = this.state
     return (
       <div>
-      hi2
+      hi3
         {datas}
         {cards}
         {posts}
+
+          <label for="suit">Suit:</label>
+          <input type="number" id="suit" name="suit"/>
+          <label for="value">Value:</label>
+          <input type="number" id="value" name="value"/>
+
+        <button onClick={() => this.testPost()} id="testPost" className="runButton">Post</button>
+
       </div>
     )
   }
